46d60e80a387be2751fe6355ea59f52d8df3743c
nikhilonwords <ceo@Onwords.in>
2025-02-28 04:58:03 +0530

    home page v0.1

--- app.py
+++ app.py
@@ -9,9 +9,11 @@ def hello_world():  # put application's code here
     return render_template('index.html')
 
 
+
+
 if __name__ == '__main__':
     # Development
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 else:
     # Production
     app.config['DEBUG'] = False
